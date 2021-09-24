60aa4eac67fd7bf0f445e80f8b4d4b7778cbfec0
Euan Ramsay <euan.ramsay@ros.gov.uk>
2021-09-24 16:15:12 +0100

    first commit

---
+++ app/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+// Component
+// State
+// Lifecycle
+// UI
+
+class App extends React.Component {
+  render() {
+    return <div>Hello World!</div>
+  }
+}
+
+ReactDOM.render(
+  // React Element
+  // Where to render the element to
+  <App />,
+  document.getElementById('app')
+)
